693593d83169f8bee48ac89339a92c9c5ef68423
Rezza Alfat <rezzaharya@students.amikom.ac.id>
2026-02-09 18:44:17 +0700

    Refactor next.config.ts for cleaner syntax

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  output: 'export', // Penting agar jadi HTML statis
-  basePath: '/portofolio-si', // Harus sama dengan nama repositori kamu
-  images: {
-    unoptimized: true, // Agar foto-foto temanmu tetap muncul
-  },
+  output: "export",
+  basePath: "/portofolio-si",
+  images: { unoptimized: true },
 };
-
 export default nextConfig;
