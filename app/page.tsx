c514e387aed1b3ac5d31457d0c8423ff9d733446
RezzaHarya <rezzaharya@students.amikom.ac.id>
2026-02-09 17:43:15 +0700

    update oleh rezza

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
-// Import ikon yang dibutuhkan (Instagram, GitHub, LinkedIn, Email)
+// Import ikon yang dibutuhkan
 import { SiLaravel, SiMysql, SiNextdotjs, SiTypescript, SiInstagram, SiGithub, SiLinkedin } from "react-icons/si";
-import { MdEmail, MdArrowForward } from "react-icons/md"; // Tambah panah untuk tombol
-
+import { MdEmail, MdArrowForward } from "react-icons/md"; 
 
 export default function Home() {
   return (
@@ -11,11 +10,17 @@ export default function Home() {
         <div className="hero-text">
           <h1 style={{ fontSize: '60px', marginBottom: '20px' }}>
             Hi, Saya <br />
-            {/* Nama diberi efek gradasi */}
             <span className="text-gradient">Rezza Alfat</span> 👋
           </h1>
 
-          <p style={{ fontSize: '18px', color: '#a1a1aa', maxWidth: '500px', lineHeight: '1.8' }}>
+          {/* PERBAIKAN: Ditambahkan margin: '0 auto' agar teks max-500px ini mau ke tengah */}
+          <p style={{ 
+            fontSize: '18px', 
+            color: '#a1a1aa', 
+            maxWidth: '500px', 
+            lineHeight: '1.8',
+            margin: '0 auto 30px auto' // Update di sini
+          }}>
             Mahasiswa Sistem Informasi yang fokus pada pengembangan
             aplikasi web modern. Spesialis dalam arsitektur skalabel menggunakan ekosistem 
             <span style={{color: '#38bdf8', fontWeight: 'bold'}}> Laravel & MySQL</span>.
@@ -37,15 +42,13 @@ export default function Home() {
             </span>
           </div>
           
-          {/* TOMBOL BARU */}
+          {/* TOMBOL */}
           <div className="hero-btn">
-            {/* Tombol Neon dengan Panah */}
             <a href="/projects" className="btn-neon">
               Lihat Project <MdArrowForward size={20} />
             </a>
             
-            {/* Tombol GitHub Dark */}
-            <a href="https://github.com/username" className="btn-dark">
+            <a href="https://github.com/RezzaHarya" className="btn-dark">
               <SiGithub size={20} /> GitHub
             </a>
           </div>
@@ -60,11 +63,10 @@ export default function Home() {
         />
       </main>
 
-      {/* --- BAGIAN FOOTER BARU --- */}
+      {/* FOOTER */}
       <footer className="footer">
         <p>© 2026 Rezza Alfat. All rights reserved.</p>
         
-        {/* Ikon Sosial Media (Urutan: IG, Github, LinkedIn, Email) */}
         <div className="social-links">
           <a href="https://instagram.com/zzaref.al_?igsh=MXNnZDV3MnB3MWgzNA==" target="_blank" rel="noopener noreferrer">
             <SiInstagram size={24} />
