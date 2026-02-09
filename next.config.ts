89f748130263b1e888a6f598ee56a038fd215495
RezzaHarya <rezzaharya@students.amikom.ac.id>
2026-02-09 17:55:51 +0700

    up lagi oleh rezza

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Penting agar jadi HTML statis
+  basePath: '/portofolio-si', // Harus sama dengan nama repositori kamu
+  images: {
+    unoptimized: true, // Agar foto-foto temanmu tetap muncul
+  },
 };
 
 export default nextConfig;
